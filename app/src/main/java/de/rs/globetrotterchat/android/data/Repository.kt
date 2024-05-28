dd1752e4a0272a1c092b923005a6f3beb7ae8d4d
reneschwarz <rene.schwarz1989@gmail.com>
2024-05-28 10:08:43 +0200

    FirebaseService created

--- app/src/main/java/de/rs/globetrotterchat/android/data/Repository.kt
+++ app/src/main/java/de/rs/globetrotterchat/android/data/Repository.kt
@@ -1,4 +1,20 @@
 package de.rs.globetrotterchat.android.data
 
-class Repository {
+import com.google.firebase.ktx.Firebase
+import de.rs.globetrotterchat.android.data.remote.FirebaseService
+
+class Repository(private val firebaseService: FirebaseService) {
+
+    suspend fun createUser(email: String, password: String): Boolean{
+        return firebaseService.createUserWithEmailAndPassword(email,password)
+    }
+
+    suspend fun signInUser(email: String,password: String):Boolean{
+        return firebaseService.signInWithEmailAndPassword(email,password)
+    }
+
+    fun signOut(){
+        firebaseService.signOut()
+    }
+
 }
