debd54485a2e3e474eeb226a4f5a10b21ab4d5e8
reneschwarz <rene.schwarz1989@gmail.com>
2024-05-31 13:24:21 +0200

    set connection FirestoreService
    -creat setProfile

--- app/src/main/java/de/rs/globetrotterchat/android/data/Repository.kt
+++ app/src/main/java/de/rs/globetrotterchat/android/data/Repository.kt
@@ -1,3 +1,16 @@
 package de.rs.globetrotterchat.android.data
 
-class Repository
+import android.util.Log
+import de.rs.globetrotterchat.android.data.model.Profile
+import de.rs.globetrotterchat.android.data.remote.FirestoreService
+
+class Repository(private val firestoreService: FirestoreService){
+
+    suspend fun setProfile(profile: Profile){
+        try {
+            firestoreService.setProfile(profile)
+        } catch (e: Exception){
+            Log.e(Repository::class.simpleName,"Could not set profile $profile: $e")
+        }
+    }
+}
