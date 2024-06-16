72cadca3a52ad4ab55313cac86351a1a855932a0
reneschwarz <rene.schwarz1989@gmail.com>
2024-06-16 19:11:40 +0200

    Fix some small issues ... install drop_down_item for NativeLanguage

--- app/src/main/java/de/rs/globetrotterchat/android/ui/main/SettingsFragment.kt
+++ app/src/main/java/de/rs/globetrotterchat/android/ui/main/SettingsFragment.kt
@@ -1,21 +1,22 @@
 package de.rs.globetrotterchat.android.ui.main
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.activityViewModels
-import de.rs.globetrotterchat.android.data.remote.FirestoreProfileService
-import de.rs.globetrotterchat.android.data.remote.FirestoreStorageService
+import de.rs.globetrotterchat.android.R
 import de.rs.globetrotterchat.android.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
     private val viewModel : MainViewModel by activityViewModels()
     private lateinit var binding: FragmentSettingsBinding
-    private val loggedInUserId = viewModel.userProfile.value!!.uid!!
+
+    private val languageCodes = arrayOf("en", "zh", "hi", "es", "fr", "ar", "bn", "ru", "pt", "ur", "id", "de", "ja", "sw", "tr", "it", "pl", "nl", "th", "gu")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentSettingsBinding.inflate(inflater)
@@ -28,19 +29,30 @@ class SettingsFragment : Fragment() {
         viewModel.userProfile.observe(viewLifecycleOwner){userProfile ->
             userProfile?.let {
                 binding.etNickName.setText(it.nickname)
-                binding.etNativeLanguage.setText(it.nativeLanguage)
+                binding.spNativeLanguage.setText(it.nativeLanguage)
             }
         }
 
         binding.ivProfile.setOnClickListener {
-            //showImagePickerOptions()
         }
+
+        val languages = resources.getStringArray(R.array.native_language_options)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, languages)
+        (binding.spNativeLanguage).apply {
+            setAdapter(adapter)
+            setOnItemClickListener { _, _, position, _ ->
+                val selectedLanguageCode = languageCodes[position]
+                // Hier kannst du den ausgewählten Sprachcode speichern oder verwenden
+            }
+        }
+
+
 
         binding.btnSaveProfile.setOnClickListener{
 
             viewModel.setProfile(
                 binding.etNickName.text.toString(),
-                binding.etNativeLanguage.text.toString())
+                binding.spNativeLanguage.text.toString())
         }
 
         binding.btnLogout.setOnClickListener {
